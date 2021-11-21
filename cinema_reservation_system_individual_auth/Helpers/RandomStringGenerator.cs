45fe68ab860b5867e086f7a289585269f34724c1
appvinio-filip-gawel <filip.gawel@appvinio.com>
2021-11-21 22:00:59 +0100

    stronger password generator

--- cinema_reservation_system_individual_auth/Helpers/RandomStringGenerator.cs
+++ cinema_reservation_system_individual_auth/Helpers/RandomStringGenerator.cs
@@ -8,7 +8,7 @@ namespace cinema_reservation_system_individual_auth.Helpers
         private static Random random = new Random();
         public static string RandomString(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
