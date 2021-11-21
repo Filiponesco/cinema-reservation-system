864a8cea1328587dc5e176f8fe574563aad5ca72
appvinio-filip-gawel <filip.gawel@appvinio.com>
2021-11-21 23:31:21 +0100

    add test of ceasar cipher

---
+++ cinema_reservation_system_individual_auth.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace cinema_reservation_system_individual_auth.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CeasarCispher_EncipherTest1234WithKey13_ReturnsEncryptedPasswordGrfg1234()
+        {
+            // arange
+            int key = 13;
+            string password = "Test1234";
+
+            // act
+            string result = CaesarCipher.Encipher(password, key);
+
+            // assert
+            Assert.Equal("Grfg1234", result);
+        }
+    }
+}
